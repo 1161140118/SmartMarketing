36c49b49d5f32e9c2eaad2fe7776a5dd1e5235d8
1161140118 <386506287@qq.com>
2020-03-26 23:12:35 +0800

    chore: add shell cmd.

--- bin/load_ti_ib_indiv_base_d.sh
+++ bin/load_ti_ib_indiv_base_d.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+
 # $1: data date / "*" or "all" : all over the date.
 
 spark-submit \
---class smk.dataops.LoadTiUbGsmBsD \
+--class smk.dataops.LoadTiIbIndivBaseD \
 --queue suyan \
 --master yarn ~/chenzhihao/smartmarketing.jar "$1" \
---executor-memory 2G\
+--num-executors 2 \
+--executor-cores 2 \
+--executor-memory 8G \
 --deploy-mode cluster
